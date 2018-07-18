6e2af83f5c769aa03847f01255ad39f488bebddd
Mahmoud <mahmoud.haouachi@gmail.com>
2018-07-18 13:27:47 +0200

    deploy

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -3,7 +3,7 @@
 #update
 yum update
 #java
-sudo yum install java-1.8.0-openjdk
-sudo yum install java-1.8.0-openjdk-devel
+sudo yum install -y java-1.8.0-openjdk
+sudo yum install -y java-1.8.0-openjdk-devel
 
 
