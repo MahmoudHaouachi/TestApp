1b103670bd15e32ea6c4a3f21f8b08558abf7a0c
Mahmoud <mahmoud.haouachi@gmail.com>
2018-07-19 12:48:14 +0200

    java fix

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -2,26 +2,14 @@
 sudo su
 #update
 yum update
-#java
+
 # Script to Install JAVA 8 (JDK 8u40) on CentOS/RHEL 7/6/5 and Fedora
 echo "Installing Java 8 (64bit)"
 
-cd /opt/
+yum install -y java-1.8.0-openjdk
 
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u40-b25/jdk-8u40-linux-x64.tar.gz"
-tar xzf jdk-8u40-linux-x64.tar.gz
+yum remove -y java-1.7.0-openjdk
 
-cd /opt/jdk1.8.0_40/
-
-alternatives --install /usr/bin/java java /opt/jdk1.8.0_40/bin/java 2
-alternatives --config java
-
-alternatives --install /usr/bin/jar jar /opt/jdk1.8.0_40/bin/jar 2
-alternatives --install /usr/bin/javac javac /opt/jdk1.8.0_40/bin/javac 2
-alternatives --set jar /opt/jdk1.8.0_40/bin/jar
-alternatives --set javac /opt/jdk1.8.0_40/bin/javac 
-
-export JAVA_HOME=/opt/jdk1.8.0_40
 
 
 # maven
