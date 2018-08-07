f89cbf5039fb1210b54b67f8812e187d24684521
Mahmoud <mahmoud.haouachi@gmail.com>
2018-08-07 12:41:05 +0200

    fix

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -46,14 +46,7 @@ echo 'Removing the temporary directory...'
 sudo rm -r "$TEMPORARY_DIRECTORY"
 echo 'Your Maven Installation is Complete.'
 
-#node
-sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
-. ~/.nvm/nvm.sh
-
-nvm install 8.11.2
-
-nvm install --lts
 
 #install mysql
 sudo yum install -y mysql-server
