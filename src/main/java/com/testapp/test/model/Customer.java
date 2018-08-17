4feb5ed73b3a1fd2a3434fc049c47cc1aa16cb3b
MahmoudHaouachi <39001429+MahmoudHaouachi@users.noreply.github.com>
2018-08-17 13:20:14 +0200

    Update Customer.java

--- src/main/java/com/testapp/test/model/Customer.java
+++ src/main/java/com/testapp/test/model/Customer.java
@@ -53,7 +53,7 @@ import javax.persistence.Table;
         }
 
         public Customer(String firstName, String lastName) {
-            this.firstName =   firstName;
+            this.firstName =   "Mr" + firstName;
             this.lastName =  lastName;
             
         }
